2f7a01d300505ce2d7b063283316a3186712186e
Nandiniatri <atrinandini8@gmail.com>
2025-04-21 20:56:14 +0530

    pushed code

--- src/notes-app/Notes.jsx
+++ src/notes-app/Notes.jsx
@@ -7,16 +7,35 @@ import { FiMoreVertical } from "react-icons/fi";
 import { FiMoreHorizontal } from "react-icons/fi";
 import ReactQuill from "react-quill";
 import "react-quill/dist/quill.snow.css";
+import { MdCheck } from "react-icons/md";
+
 
 const Notes = () => {
     const [notes, setNotes] = useState([]);
     const { bgColor, setShowModal, showModal } = useContext(allDataContext);
 
+    const modules = {
+        toolbar: [
+            ['bold', 'italic', 'underline'],
+            [{ list: 'bullet' }]
+        ]
+    }
+
+
+
+    const saveNote = () => {
+
+    }
 
     const handleNotesAdd = () => {
         setNotes((prevNotes) => [
             ...prevNotes,
-            { id: Date.now(), bgColor: "white", quill: <ReactQuill /> }
+            {
+                id: Date.now(), bgColor: "gray", quill: <ReactQuill
+                    theme="snow"
+                    modules={modules}
+                />
+            }
         ]);
     };
 
@@ -38,44 +57,53 @@ const Notes = () => {
     };
 
     return (
-        <div>
-            <Button onClick={handleNotesAdd} className="noteBtn">+ Note</Button>
+        <div className="note-main-container-div">
+            <div className="div1">
+                <Button onClick={handleNotesAdd} className="noteBtn">+ Note</Button>
+                <div className="notes-main-container">
+                    {notes.map((note) => {
+                        return (
+                            <div
+                                key={note.id}
+                                className="notes-inside-div"
+                            >
 
-            <div className="notes-main-container">
-                {notes.map((note) => {
-                    return (
-                        <div
-                            key={note.id}
-                            className="notes-inside-div"
-                            style={{ backgroundColor: note.bgColor }}
-                        >
+                                <div className="quill-container-div" style={{ backgroundColor: note.bgColor }}>{note.quill}</div>
 
-                            <div>{note.quill}</div>
+                                <div className="button-div">
+                                    <p onClick={() => handleThreeDot(note.id)} className="btn-div1"> <FiMoreHorizontal size={20} /></p>
 
-                            <div className="button-div">
-                                <p onClick={() => handleThreeDot(note.id)} className="btn-div1"> <FiMoreHorizontal size={20} /></p>
+                                    <p onClick={() => handleNoteDel(note.id)} className="btn-div2"><TiDelete size={23} /></p>
 
-                                <p onClick={() => handleNoteDel(note.id)} className="btn-div2"><TiDelete size={23} /></p>
+                                    <p onClick={saveNote}><MdCheck size={24} /></p>
+                                </div>
+
+                                <div>
+                                    {showModal && (
+                                        <div className="showModal-div">
+                                            {bgColor.map((item) => (
+                                                <div
+                                                    key={item.id}
+                                                    className="color-box"
+                                                    style={{ backgroundColor: item.backgroundColor }}
+                                                    onClick={() => handleBgColor(item.backgroundColor, note.id)}
+                                                ></div>
+                                            ))}
+                                        </div>
+                                    )}
+                                </div>
                             </div>
+                        );
+                    })}
+                </div>
 
-                            <div>
-                                {showModal && (
-                                    <div className="showModal-div">
-                                        {bgColor.map((item) => (
-                                            <div
-                                                key={item.id}
-                                                className="color-box"
-                                                style={{ backgroundColor: item.backgroundColor }}
-                                                onClick={() => handleBgColor(item.backgroundColor, note.id)}
-                                            ></div>
-                                        ))}
-                                    </div>
-                                )}
-                            </div>
-                        </div>
-                    );
-                })}
             </div>
+
+
+            <div className="div2">
+                <h4>Recent notes</h4>
+            </div>
+
         </div>
     );
 };
